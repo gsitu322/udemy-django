f085bcba00bd847bf4825e7eeed02c37f9c432ba
Gordon Situ <gsitu322@gmail.com>
2025-08-23 15:15:24 -0700

    Registering app to load html views

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import challenges
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
