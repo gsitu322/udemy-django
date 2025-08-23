5039441b5d457fb51a2849735611c8d569587fc0
Gordon Situ <gsitu322@gmail.com>
2025-08-23 00:45:36 -0700

    Added dynamic path handling

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "January"
+    elif month == "february":
+        challenge_text = "February"
+    elif month == "march":
+        challenge_text = "March"
+    else:
+        return HttpResponseNotFound("Incorrect month provided")
+
+    return HttpResponse(f"Hello, world. {challenge_text}")
